91a4938b12bfd57f2ed568eefe6dc3335d08891e
minjeong <cookie00421@gmail.com>
2025-03-16 20:56:52 +0900

    feat: 마이페이지 UI 구현

--- src/components/common/Icons/index.tsx
+++ src/components/common/Icons/index.tsx
@@ -3,3 +3,5 @@ export { Calendar as CalendarIcon } from "./Calendar";
 export { Bulb as BulbIcon } from "./Bulb";
 export { Logo as LogoIcon } from "./Logo";
 export { ExclamationMark as ExclamationMarkIcon } from "./ExclamationMark";
+export { Flame as FlameIcon } from "./Flame";
+export { Medal as MedalIcon } from "./Medal";
