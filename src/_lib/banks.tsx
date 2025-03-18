085133ffa3059a1aa672fe42384a393d3749ea19
minjeong <cookie00421@gmail.com>
2025-03-19 01:19:43 +0900

    feat: 회원가입 로직 API 연동

---
+++ src/_lib/banks.tsx
@@ -0,0 +1,74 @@
+// 모든 은행 이미지 불러오기
+import ibk from "src/assets/images/banks/ibk.png";
+import hana from "src/assets/images/banks/hana.png";
+import toss from "src/assets/images/banks/toss.png";
+import kbank from "src/assets/images/banks/kbank.png";
+import kakaobank from "src/assets/images/banks/kakaobank.png";
+import post from "src/assets/images/banks/post.png";
+import woori from "src/assets/images/banks/woori.png";
+import shinhyup from "src/assets/images/banks/shinhyup.png";
+import shinhan from "src/assets/images/banks/shinhan.png";
+import citi from "src/assets/images/banks/citi.png";
+import suhyup from "src/assets/images/banks/suhyup.png";
+import saemaul from "src/assets/images/banks/saemaul.png";
+import nonghyup from "src/assets/images/banks/nonghyup.png";
+import kb from "src/assets/images/banks/kb.png";
+
+export const BANKS = [
+  {
+    name: "국민은행",
+    image: kb,
+  },
+  {
+    name: "신한은행",
+    image: shinhan,
+  },
+  {
+    name: "우리은행",
+    image: woori,
+  },
+  {
+    name: "하나은행",
+    image: hana,
+  },
+  {
+    name: "카카오뱅크",
+    image: kakaobank,
+  },
+  {
+    name: "케이뱅크",
+    image: kbank,
+  },
+  {
+    name: "토스뱅크",
+    image: toss,
+  },
+  {
+    name: "IBK기업은행",
+    image: ibk,
+  },
+  {
+    name: "우체국은행",
+    image: post,
+  },
+  {
+    name: "신협",
+    image: shinhyup,
+  },
+  {
+    name: "시티은행",
+    image: citi,
+  },
+  {
+    name: "수협은행",
+    image: suhyup,
+  },
+  {
+    name: "새마을금고",
+    image: saemaul,
+  },
+  {
+    name: "농협은행",
+    image: nonghyup,
+  },
+];
